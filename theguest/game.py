0680ae4819b40c5f6e9b3132cf6ac8d439218aa4
Boncroix <boncroix@gmail.com>
2023-10-29 21:18:34 +0100

    fear: hacer que la instancia de las escenas no se cree al inicio del bucle

--- theguest/game.py
+++ theguest/game.py
@@ -21,27 +21,26 @@ class TheGuest:
 
     def jugar(self):
         self.escena_selec = 'portada'
-
         while True:
-            self.escenas = {
-                'portada': Portada(self.pantalla, self.sonido_activo),
-                'partida': Partida(self.pantalla, self.dificultad, self.vidas, self.puntos, self.nivel, self.sonido_activo),
-                'records': Records(self.pantalla, self.sonido_activo)
-            }
             if self.escena_selec == 'portada':
                 self.musica = pg.mixer.music.load(MUSICA['portada'])
                 self.musica = pg.mixer.music.play(-1)
-                self.escena_selec, self.sonido_activo = self.escenas[self.escena_selec].bucle_principal(
+                self.escena = Portada(self.pantalla, self.sonido_activo)
+                self.escena_selec, self.sonido_activo = self.escena.bucle_principal(
                 )
                 self.musica = pg.mixer.music.load(MUSICA['partida'])
                 self.musica = pg.mixer.music.play(-1)
             elif self.escena_selec == 'partida':
-                self.escena_selec, self.dificultad, self.vidas, self.puntos, self.nivel, self.sonido_activo = self.escenas[self.escena_selec].bucle_principal(
+                self.escena = Partida(self.pantalla, self.dificultad,
+                                      self.vidas, self.puntos, self.nivel, self.sonido_activo)
+                self.escena_selec, self.dificultad, self.vidas, self.puntos, self.nivel, self.sonido_activo = self.escena.bucle_principal(
                 )
             elif self.escena_selec == 'records':
                 self.musica = pg.mixer.music.load(MUSICA['records'])
                 self.musica = pg.mixer.music.play(-1)
-                self.escena_selec, self.sonido_activo = self.escenas[self.escena_selec].bucle_principal(
+                self.escena = Records(
+                    self.pantalla, self.sonido_activo, self.puntos)
+                self.escena_selec, self.sonido_activo, self.puntos = self.escena.bucle_principal(
                 )
             elif self.escena_selec == 'salir':
                 pg.mixer.music.stop()
